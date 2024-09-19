f23f4207f5281e0eabd6df48545c2d95f259e37f
it.delements <it@delements.co>
2024-09-19 19:03:36 +0700

    remove file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
+        {/* <Image
           className="dark:invert"
           src="https://rootrotation.com/cdn/shop/files/custom_resized_c67e340d-b54d-473e-9f6a-6d6fafab3017.png?v=1664474570"
           alt="Next.js logo"
           width={180}
           height={1}
           priority
-        />
+        /> */}
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">lorem lorem lorem lorem</li>
           <li>lorem lorem</li>

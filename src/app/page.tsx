1f39be9a06dc658203b980f380832c2153f5d39d
it.delements <it@delements.co>
2024-09-23 18:29:11 +0700

    chore : fix url purchase plan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {/* <Image
+        <Image
           className="dark:invert"
-          src="https://rootrotation.com/cdn/shop/files/custom_resized_c67e340d-b54d-473e-9f6a-6d6fafab3017.png?v=1664474570"
+          src="https://rootrotation.com/cdn/shop/files/AVFDGXFDC_HJM.png?v=1678361915&width=760"
           alt="Next.js logo"
           width={180}
           height={1}
           priority
-        /> */}
+        />
+
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">lorem lorem lorem lorem</li>
           <li>lorem lorem</li>
@@ -30,7 +32,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://docs.google.com/forms/d/e/1FAIpQLSferf3xJxB0X8A8gGuF-ea390UX6u7NVOIlUlW0oxE6Iau-pA/viewform"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfU0Fb1oO-SlkTbcQi4nVFbYtFuOFkViJEAaLLkFvu8TwpTqg/viewform"
           >
             Go to Purchase Plan
           </Link>

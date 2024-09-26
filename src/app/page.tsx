7a993b5258367771a030dfe7253e609d5a2ff5f1
it.delements <it@delements.co>
2024-09-26 14:54:57 +0700

    feat : update button + background + add image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,20 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-   
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-        
+          <img
+            src="\assets\images\rr.png"
+            alt="Error! Page not found"
+            title="Error! Page not found"
+            className=" max-width: 100%; max-height: 60vh; margin: 0 auto;"
+          ></img>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-        <Link
+          <Link
             target="_blank"
             rel="noopener noreferrer"
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="rounded-full border border-solid border-black text-black hover:bg-black hover:text-white transition-colors flex items-center justify-center hover:border-black text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://docs.google.com/forms/d/e/1FAIpQLSfU0Fb1oO-SlkTbcQi4nVFbYtFuOFkViJEAaLLkFvu8TwpTqg/viewform"
           >
             Go to Purchase Plan
@@ -21,7 +25,7 @@ export default function Home() {
           <Link
             target="_blank"
             rel="noopener noreferrer"
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="rounded-full border border-solid border-black text-black hover:bg-black hover:text-white transition-colors flex items-center justify-center hover:border-black text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://docs.google.com/forms/d/e/1FAIpQLSferf3xJxB0X8A8gGuF-ea390UX6u7NVOIlUlW0oxE6Iau-pA/viewform"
           >
             Go to WAREHOUSE IMPORT - EXPORT PLAN

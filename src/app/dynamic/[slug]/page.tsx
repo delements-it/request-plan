0b166c29cc4af47a3c5473ad82ad6d0f9631ea56
it.delements <it@delements.co>
2025-02-24 13:41:15 +0700

    feat : test link dynamic

---
+++ src/app/dynamic/[slug]/page.tsx
@@ -0,0 +1,16 @@
+import { redirect } from 'next/navigation';
+
+export default function DynamicRedirect({ params }: { params: { slug: string } }) {
+  const destinations: { [key: string]: string } = {
+    dynamicz: 'https://zingnews.vn',
+  };
+
+  const destination = destinations[params.slug];
+
+  if (!destination) {
+    // Trả về trang 404 nếu không tìm thấy URL đích
+    return new Response('Not Found', { status: 404 });
+  }
+
+  redirect(destination);
+}

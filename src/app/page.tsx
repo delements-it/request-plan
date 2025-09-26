8620db0bbcc1d996249a7a32d7ab94e98438a5e3
delements-it <128577457+delements-it@users.noreply.github.com>
2025-09-26 16:56:50 +0700

    update: request plan page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
             <span className="relative z-10">Purchase Plan</span>
             <span className="absolute inset-0 border-2 rounded-full border-transparent group-hover:rounded-[25px] group-hover:bg-gradient-to-r group-hover:from-[#E9BB56] group-hover:via-[#C95B4F] group-hover:via-[#398EBB] group-hover:to-[#257C61] group-hover:bg-[length:200%_100%] group-hover:animate-border-slide pointer-events-none" style={{ WebkitMask: 'linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)', WebkitMaskComposite: 'xor', maskComposite: 'exclude' }}></span>
           </Link> */}
-          <Link
+          {/* <Link
             target="_blank"
             rel="noopener noreferrer"
             className="rounded-full border-2 border-gray-300 text-black flex items-center justify-center text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44 text-center group relative overflow-hidden transition-all duration-300 hover:rounded-[25px] hover:border-transparent"
@@ -31,7 +31,7 @@ export default function Home() {
           >
             <span className="relative z-10">WAREHOUSE IMPORT - EXPORT PLAN</span>
             <span className="absolute inset-0 border-2 rounded-full border-transparent group-hover:rounded-[25px] group-hover:bg-gradient-to-r group-hover:from-[#E9BB56] group-hover:via-[#C95B4F] group-hover:via-[#398EBB] group-hover:to-[#257C61] group-hover:bg-[length:200%_100%] group-hover:animate-border-slide pointer-events-none" style={{ WebkitMask: 'linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)', WebkitMaskComposite: 'xor', maskComposite: 'exclude' }}></span>
-          </Link>
+          </Link> */}
           <Link
             target="_blank"
             rel="noopener noreferrer"
@@ -41,7 +41,7 @@ export default function Home() {
             <span className="relative z-10">SKU Creation Request Form</span>
             <span className="absolute inset-0 border-2 rounded-full border-transparent group-hover:rounded-[25px] group-hover:bg-gradient-to-r group-hover:from-[#E9BB56] group-hover:via-[#C95B4F] group-hover:via-[#398EBB] group-hover:to-[#257C61] group-hover:bg-[length:200%_100%] group-hover:animate-border-slide pointer-events-none" style={{ WebkitMask: 'linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)', WebkitMaskComposite: 'xor', maskComposite: 'exclude' }}></span>
           </Link>
-          <Link
+          {/* <Link
             target="_blank"
             rel="noopener noreferrer"
             className="rounded-full border-2 border-gray-300 text-black flex items-center justify-center text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44 text-center group relative overflow-hidden transition-all duration-300 hover:rounded-[25px] hover:border-transparent"
@@ -67,7 +67,7 @@ export default function Home() {
           >
             <span className="relative z-10">Sample Production Request</span>
             <span className="absolute inset-0 border-2 rounded-full border-transparent group-hover:rounded-[25px] group-hover:bg-gradient-to-r group-hover:from-[#E9BB56] group-hover:via-[#C95B4F] group-hover:via-[#398EBB] group-hover:to-[#257C61] group-hover:bg-[length:200%_100%] group-hover:animate-border-slide pointer-events-none" style={{ WebkitMask: 'linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)', WebkitMaskComposite: 'xor', maskComposite: 'exclude' }}></span>
-          </Link>
+          </Link> */}
         </div>
       </main>
     </div>
